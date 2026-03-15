45a4a22fec78bf12416120a49e09f32c164a9b75
Vladimir Nikulin <mail.jorey@gmail.com>
2026-03-16 00:14:17 +0300

    Removing unnecessary files

--- js/reset_focus_on_the_page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(() => {
-  // src/ts/reset_focus_on_the_page.ts
-  window.onpageshow = function(event) {
-    if (event.persisted) {
-      window.location.reload();
-    }
-  };
-})();
